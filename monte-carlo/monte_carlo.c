982c30f3d1f19c919dba74a58233c8aabd641e29
Chanyoung Lee <btstar1998@gmail.com>
2023-11-15 15:31:39 +0900

    Implement Monte carlo

--- monte-carlo/monte_carlo.c
+++ monte-carlo/monte_carlo.c
@@ -12,18 +12,24 @@ double monte_carlo(double *xs, double *ys, int num_points, int mpi_rank, int mpi
   // In total, (mpi_world_size * threads_per_process) threads will collaborate
   // to compute pi.
 
-  if (mpi_rank == 0) {
-    for (int i = 0; i < num_points; i++) {
+  MPI_Bcast(xs, num_points, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Bcast(ys, num_points, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+  #pragma omp parallel num_threads(threads_per_process)
+  {
+    #pragma omp for reduction(+:count)
+    for (int i = mpi_rank; i < num_points; i += mpi_world_size) {
       double x = xs[i];
       double y = ys[i];
 
-      if (x*x + y*y <= 1)
-        count++;
+      if (x * x + y * y <= 1) count++;
     }
   }
 
+  int total_count = 0;
+  MPI_Reduce(&count, &total_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
   // Rank 0 should return the estimated PI value
   // Other processes can return any value (don't care)
-  return (double) 4 * count / num_points;
+  return (double) 4 * total_count / num_points;
 }
